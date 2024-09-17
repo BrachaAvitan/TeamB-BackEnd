06d713a79eafc3e84b619808319cad3e74b82114
Ahuva1234 <Ahuva2926@gmail.com>
2024-09-17 22:11:34 +0300

    initial project backend

---
+++ App.Books/App.DA/DataContext/BooksContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using App.DA.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.DA.DataContext;
+
+public partial class BooksContext : DbContext
+{
+  
+
+    public BooksContext(DbContextOptions<BooksContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(e => e.bookId).HasName("PK__books__B40CC6CDD34A6E46");
+
+            entity.Property(e => e.bookId).ValueGeneratedNever();
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.title).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ App.Books/App.DTO/Models/BookDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App.DA.Entities;
+
+public partial class BookDTO
+{
+
+    public Guid bookId { get; set; }
+
+    public string? anthor { get; set; }
+
+    public string? title { get; set; }
+    public int publishYear { get; set; }
+    public decimal Price { get; set; }
+
+
+}
